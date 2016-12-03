41b64dc7b398bfd3ce12291e98a4d9613b6c3409
KFadel <karimm.fadel@yahoo.com>
2016-12-03 23:24:47 +0200

    start

---
+++ src/main/java/com/tornado/ecommerce/security/CustomAuthenticationProvider.java
@@ -0,0 +1,29 @@
+package com.tornado.ecommerce.security;
+
+import javax.annotation.Resource;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+public class CustomAuthenticationProvider implements AuthenticationProvider{
+
+	@Override
+	public Authentication authenticate(Authentication authentication)
+			throws AuthenticationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean supports(Class<?> authentication) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/main/java/com/tornado/ecommerce/security/RestUsernamePasswordAuthenticationFilter.java
@@ -0,0 +1,85 @@
+package com.tornado.ecommerce.security;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class RestUsernamePasswordAuthenticationFilter extends
+		UsernamePasswordAuthenticationFilter {
+
+	@Autowired
+	private CustomAuthenticationProvider customAuthenticationProvider;
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request,
+			HttpServletResponse response) throws AuthenticationException {
+
+		String username = obtainUsername(request);
+		String password = obtainPassword(request);
+
+		if (username == null) {
+			username = "";
+		}
+
+		if (password == null) {
+			password = "";
+		}
+
+		username = username.trim();
+
+		UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(
+				username, password);
+
+		// Allow subclasses to set the "details" property
+		setDetails(request, authRequest);
+
+		return this.customAuthenticationProvider.authenticate(authRequest);
+	}
+
+	@Override
+	protected String obtainPassword(HttpServletRequest request) {
+		return request.getHeader("j_password");
+	}
+
+	@Override
+	protected String obtainUsername(HttpServletRequest request) {
+		return request.getHeader("j_username");
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+		Authentication auth = attemptAuthentication(httpRequest, httpResponse);
+		SecurityContextHolder.getContext().setAuthentication(auth);
+
+		chain.doFilter(request, response);
+
+	}
+	
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult)
+            throws IOException, ServletException {
+        super.successfulAuthentication(request, response, chain, authResult);
+
+        // As this authentication is in HTTP header, after success we need to continue the request normally
+        // and return the response as if the resource was not secured at all
+        chain.doFilter(request, response);
+    }
+
+}
